baab854c2989f32ef26ea773e3419990a5aff7f4
Ignashov Vadim Maksimovich <48078801+ImSOLty@users.noreply.github.com>
2024-10-24 16:25:59 +0300

    Add test runner workflow

--- tests/tests.py
+++ tests/tests.py
@@ -14,21 +14,23 @@ def load_env():
     load_dotenv()
 
 
-@pytest.fixture(scope="module")
 def ya_api_token():
     return os.getenv(TOKEN_ENV)
 
 
 @pytest.mark.parametrize('breed', ['doberman', random.choice(['bulldog', 'collie'])])
-def test_proverka_upload_dog(ya_api_token, breed):
-    u(breed, ya_api_token)
+def test_proverka_upload_dog(breed):
+    token = ya_api_token()
+    u(breed, token)
     # проверка
     url_create = 'https://cloud-api.yandex.net/v1/disk/resources'
     headers = {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
-        'Authorization': f'OAuth {ya_api_token}'}
-    response = requests.get(f'{url_create}?path=/test_folder', headers=headers)
+        'Authorization': f'OAuth {token}'}
+    target_url = f'{url_create}?path=/test_folder'
+    response = requests.get(target_url, headers=headers)
+    assert response.ok, f"Couldn't successfully GET target url ({target_url})"
     assert response.json()['type'] == "dir"
     assert response.json()['name'] == "test_folder"
     assert True
